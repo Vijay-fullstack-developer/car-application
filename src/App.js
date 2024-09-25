cb3d75612fcc5f70982340584561cec9c5ef8758
vijay S <vijays17111997@gmail.com>
2024-09-25 08:27:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,30 +8,31 @@ import New_car from './sub-page/New_car';
 import Brands from './sub-page/Brands';
 import Contact from './sub-page/Contact';
 
-const router = createBrowserRouter([
-  {
-    path:"/home",element:<Home/>
-  },
-  {
-    path:"/service",element:<Service/>
-  },
-  {
-    path:"/featured_cars",element:<Feature_car/>
-  },
-  {
-    path:"/new_cars",element:<New_car/>
-  },
-  {
-    path:"/brands",element:<Brands/>
-  },
-  {
-    path:"/contact",element:<Contact/>
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path:"/home",element:<Home/>
+//   },
+//   {
+//     path:"/service",element:<Service/>
+//   },
+//   {
+//     path:"/featured_cars",element:<Feature_car/>
+//   },
+//   {
+//     path:"/new_cars",element:<New_car/>
+//   },
+//   {
+//     path:"/brands",element:<Brands/>
+//   },
+//   {
+//     path:"/contact",element:<Contact/>
+//   }
+// ]);
 
 export default function App() {
   return (
-    <RouterProvider router={router}/>
+    // <RouterProvider router={router}/>
+    <Home/>
    
   );
 }
